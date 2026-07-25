3a2d00b0ed03714ebd812fb0a7f26aaa58f86a4a
jaiminunagar1 <159227111+jaiminunagar1@users.noreply.github.com>
2026-07-25 17:12:47 +0530

    Refactor maxiprofit function and enhance maxiprofit2 implementation for better clarity and performance

--- 3. Problems on Arrays/3.2 MEDIUM/3_Best Time to Buy and Sell Stock.py
+++ 3. Problems on Arrays/3.2 MEDIUM/3_Best Time to Buy and Sell Stock.py
@@ -12,6 +12,23 @@ def maxiprofit(nums:list) -> int:
                     max_profit = profit
     return max_profit
 
+
+def maxiprofit2(prices:list[int])->int:
+    max_profit = 0
+    mini_price = float("inf")
+    n = len(prices)
+    for i in range(0,n):
+        if prices[i]<mini_price:
+            mini_price = prices[i]
+        if prices[i]>mini_price:
+            profit = prices[i]-mini_price
+            if profit>max_profit:
+                max_profit = profit
+    return max_profit
+
+
 if __name__ == "__main__":
     nums = [7,2,1,5,6,4,8]
-    print(maxiprofit(nums))
+    nums2 = [1,2]
+    # print(maxiprofit(nums2))
+    print(maxiprofit2(nums2))
